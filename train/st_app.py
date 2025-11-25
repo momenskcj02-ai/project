0b4318a6966811e2f9ac0015ee938d71f05d610f
3omarsaid <omarelking288@gmail.com>
2025-11-25 19:58:05 +0200

    Delete train directory

--- train/st_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("أهلاً بيك في Streamlit! 👋")
-
-name = st.text_input("اكتب اسمك:")
-age = st.number_input("اكتب سنّك:", min_value=1, max_value=120)
-
-if st.button("اعرض الرسالة"):
-    st.success(f"مرحباً {name}! سنّك هو {int(age)} سنة.")
